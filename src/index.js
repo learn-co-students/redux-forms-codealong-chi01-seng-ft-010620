be0a9949a1731b78898636a111f1019399485743
Zach Weber <weberzt@gmail.com>
2020-03-22 17:44:00 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import manageTodo from './reducers/manageTodo';
+// import CreateTodo from './components/CreateTodo'
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
-
+import { connect } from 'react-redux';
+ 
 let store = createStore(manageTodo);
-
+ 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
 );
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addTodo: formData => dispatch({ type: 'ADD_TODO', payload: formData })
+  };
+};
